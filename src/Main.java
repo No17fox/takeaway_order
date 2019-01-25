2806238e674221b37f90918430363f070468b0c9
WangLei <wanglei0717@outlook.com>
2019-01-25 11:29:09 +0800

    Add feature to parse user input

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,14 @@
+import java.util.List;
+import java.util.Map;
 
 public class Main {
 
   public static void main(String[] args) {
     Tools tools = new Tools();
-    tools.promptInput();
-    tools.readUserInput();
+    List<Dish> menu = tools.loadMenu();
+    tools.promptInput(menu);
+    String userInput = tools.readUserInput();
+    Map orderedDishes = tools.parseInput(userInput);
+    System.out.println(orderedDishes);
   }
 }
--- src/Tools.java
+++ src/Tools.java
@@ -2,14 +2,16 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Tools {
 
-  public void promptInput() {
+  public void promptInput(List<Dish> menu) {
     System.out.println("欢迎进入--吃什么鸭--点餐系统\n\n今日菜单：");
-    printMenu();
-    System.out.println("\n请输入您需要的菜品和数量（例如：黄焖鸡 x 1，冰峰 x 1）：");
+    printMenu(menu);
+    System.out.println("\n请输入您需要的菜品和数量（例如：黄焖鸡 1，冰峰 1）：");
   }
 
   public List<Dish> loadMenu() {
@@ -21,21 +23,46 @@ public class Tools {
     return menu;
   }
 
-  public void printMenu() {
-    List<Dish> menu = loadMenu();
+  public void printMenu(List<Dish> menu) {
     for (Dish dish : menu) {
       System.out.println(dish.name + "--" + dish.price + "元");
     }
   }
 
-  public void readUserInput() {
+  public String readUserInput() {
     InputStreamReader inputStreamReader = new InputStreamReader(System.in);
     BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
     try {
       String userInput = bufferedReader.readLine();
+      return userInput;
     } catch (IOException e) {
       System.out.println("Exception encountered: " + e);
+      return null;
     }
+  }
+
+  public Map parseInput(String userInput) {
+    Map orderedDishes = new HashMap();
+    if (userInput == null || userInput.isEmpty()) {
+      System.out.println("请输入您需要的菜品和数量（例如：黄焖鸡 1，冰峰 1）：");
+      userInput = readUserInput();
+      orderedDishes = parseInput(userInput);
+    } else {
+      String[] inputArray = userInput.split(",|，");
+      for (String item : inputArray) {
+        String[] itemInfor = item.split(" ");
+        if (itemInfor.length == 2 && itemInfor[0] != "" && itemInfor[1].matches("^\\d*$")) {
+          String dishName = itemInfor[0];
+          int dishCount = Integer.parseInt(itemInfor[1]);
+          orderedDishes.put(dishName, dishCount);
+        } else {
+          System.out.println("输入格式有误，请重新输入您需要的菜品和数量（例如：黄焖鸡 1，冰峰 1）：");
+          userInput = readUserInput();
+          orderedDishes = parseInput(userInput);
+        }
+      }
+    }
+    return orderedDishes;
   }
 
 }
